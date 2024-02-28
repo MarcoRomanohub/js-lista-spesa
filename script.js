0d8f67e076de2b381fcf4ae54b322331f6bbff1e
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-02-28 10:24:37 +0100

    [fixed] html and js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const lista = document.querySelector('h1');
 const listaDellaSpesa = [' Pasta ', ' Verdure', ' Pomodoro', ' Carne', ' Patate', ' Farina', ' Acqua', ' Frutta',];
+const lista = document.querySelector('.list');
 let i = 0;
 while(i < listaDellaSpesa.length) {
   console.log(listaDellaSpesa[i]);
 
-  lista.innerHTML += listaDellaSpesa[i]; 
+  lista.innerHTML += `<li>${listaDellaSpesa[i]}</li>`; 
   i++;
 }
